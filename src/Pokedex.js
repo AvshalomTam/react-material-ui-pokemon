70b641b466f1a97b5a23e841df060f49a702dec1
Avshalom <avshalom.tam@gmail.com>
2021-02-09 18:59:14 +0200

    added style

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -7,11 +7,14 @@ import {
     CardContent, 
     CardMedia, 
     CircularProgress,
-    Typography } from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
+    Typography,
+    TextField } from '@material-ui/core';
+import {fade, makeStyles} from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search'
 import axios from 'axios';
 
-const useStyles = makeStyles({
+// use material ui components to fix page look!
+const useStyles = makeStyles(theme => ({
     pokedexContainer: {
         paddingTop: '20px',
         paddingLeft: '50px',
@@ -30,8 +33,24 @@ const useStyles = makeStyles({
     },
     CardContent: {
         textAlign: 'center'
+    },
+    searchContainer: {
+        display: 'flex',
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        paddingLeft: "20px",
+        paddingRight: "20px",
+        marginTop: "5px",
+        marginBottom: "5px"
+    },
+    searchIcon: {
+        alignSelf: "flex-end",
+        marginBottom: "5px",
+    },
+    searchInput: {
+        width: "200px",
+        margin: "5px"
     }
-}) 
+}))
 
 export const firstLetterUp = (name) => {
     return name.charAt(0).toUpperCase() + name.slice(1)
@@ -41,6 +60,11 @@ const Pokedex = (props) => {
     const {history} = props;
     const classes = useStyles();
     const [pokemonData, setPokemonData] = useState({})
+    const [filter, setFilter] = useState("")
+
+    const handleSearchChange = (e) => {
+        setFilter(e.target.value)
+    }
 
     useEffect(() => {
         axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
@@ -84,11 +108,23 @@ const Pokedex = (props) => {
     return (
         <>
           <AppBar position="static">
-            <Toolbar />
+            <Toolbar>
+                <div className={classes.searchContainer}>
+                <SearchIcon className={classes.searchIcon}/>
+                <TextField 
+                className={classes.searchInput}
+                label="Pokemon"
+                variant="standard"
+                onChange={handleSearchChange}
+                />
+                </div>
+            </Toolbar>
           </AppBar>
           {pokemonData ? (
           <Grid container spacing={2} className={classes.pokedexContainer}>
-              {Object.keys(pokemonData).map(id => getPokemonCard(id))}
+              {Object.keys(pokemonData).map(id => 
+                  pokemonData[id].name.includes(filter) && getPokemonCard(id)
+                  )}
           </Grid>
           ) : (
               <CircularProgress className={classes.pokedexSearching}/>
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,11 +1,79 @@
-import React, {useState} from 'react'
-import {Typography, Link, CircularProgress, Button} from '@material-ui/core';
-// import {Link} from "react-router-dom";
+import React, {useState, useEffect} from 'react'
+import {Typography, 
+    Link, 
+    CircularProgress, 
+    Button, 
+    Container,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper    
+} from '@material-ui/core';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import {firstLetterUp} from './Pokedex'
 import axios from 'axios';
-import { useEffect } from 'react';
+
+const TABLE_COLOR = '#2F4F4F';
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: TABLE_COLOR,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+}))(TableRow);
+
+const useStyles = makeStyles({
+    pokemonContainer: {
+        color: TABLE_COLOR,
+        textAlign: 'center',
+        paddingTop: '20px'
+    },
+    pokemonName :{
+        fontSize: "40px",
+        text: 'bold'
+    },
+    table: {
+        position: 'relative',
+        maxWidth: 300
+    },
+    tableContainer: {
+        margin: 'auto',
+        position: 'relative',
+        padding: '1px',
+        maxWidth: 300,
+        maxHeight: 180,
+    },
+    tableCell: {
+        size: 'small'
+    }
+});
+
+const makeTypesString = (types) => {
+    let typesList = ''
+    types.map(typeInfo => {
+        typesList += `${typeInfo.type.name}, `
+    })
+    // subtract 2 last characters
+    typesList = typesList.slice(0,-2)
+    return typesList
+}
 
 const Pokemon = props => {
+    const classes = useStyles();
     const {history, match} = props;
     const {params} = match;
     const pokemonID = params.id
@@ -23,42 +91,67 @@ const Pokemon = props => {
     }, [pokemonID])
 
     const generatePokemonJSX = () => {
-        const {name, id, species, height, weight, types, sprites} = pokemon
+        const {name, id, species, height, weight, types} = pokemon
+        const typesAsString = makeTypesString(types)
         const fullImgUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png` 
-        const {front_default} = sprites
 
         return (
             <>
-            <Typography variant="h2">
+            <Typography className={classes.pokemonName}>
                 {`${id}. ${firstLetterUp(name)}`}
-                <img src={front_default} alt=""/>
             </Typography>
-            <img style={{width:"100px", height:"100px"}} src={fullImgUrl} alt=""/>
-            <Typography variant="h5">Pokemon Info:</Typography>
-            <Typography>
-                {"Species: "}
-                <Link to={species.url}>{species.name}</Link>
-            </Typography>
-            <Typography>Height: {height}</Typography>
-            <Typography>Weight: {weight}</Typography>
-            <Typography variant="h6">Types:</Typography>
-            {types.map(typeInfo => {
-                const name = typeInfo.type.name
-                return <Typography key={name}>{name}</Typography>
-                })}
+            <Typography variant="h5" display="inline">Type: </Typography>
+            <Typography variant="h6" display="inline" color='primary'>{typesAsString}</Typography>
+            <br></br>
+            <img style={{width:"180px", height:"180px"}} src={fullImgUrl} alt=""/>
+            <TableContainer component={Paper} className={classes.tableContainer}>
+                <Table className={classes.table} aria-label="customized table">
+                    <TableHead>
+                    <TableRow>
+                        <StyledTableCell size='small'>Pokemon Info:</StyledTableCell>
+                        <StyledTableCell align="right" size='small'>value</StyledTableCell>
+                    </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                    <StyledTableRow>
+                            <StyledTableCell size='small'>Species</StyledTableCell>
+                            <StyledTableCell align="right" size='small'>{species.name}</StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                            <StyledTableCell size='small'>Height</StyledTableCell>
+                            <StyledTableCell align="right" size='small'>{height}</StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                            <StyledTableCell size='small'>Weight</StyledTableCell>
+                            <StyledTableCell align="right" size='small'>{weight}</StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                            <StyledTableCell size='small'>Type</StyledTableCell>
+                            <StyledTableCell align="right" size='small'>{typesAsString}</StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                            <StyledTableCell size='small'>Serial Number</StyledTableCell>
+                            <StyledTableCell align="right" size='small'>{id}</StyledTableCell>
+                    </StyledTableRow>
+                    </TableBody>
+                </Table>
+                </TableContainer>
             </>
         )
     }
     return (
         <>
+        <Container className={classes.pokemonContainer}> 
         {pokemon === undefined && <CircularProgress />}
         {pokemon !== undefined && pokemon && generatePokemonJSX()}
         {pokemon === false && <Typography>Pokemon not found!</Typography>}
         {pokemon !== undefined && (
             <Button variant="contained" onClick={() => history.push('/')}>
-                Back to All Pokemons
+                Back to Pokemons
             </Button>
         )}
+        </Container>
         </>
     )
 }
